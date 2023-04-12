5e03bafd78affed4590e7a54cfd44d998b46c364
Lam Dinh <lamdinhreply@gmail.com>
2023-04-12 12:01:20 +0200

    Refactoring & added comments

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const inputCity = document.querySelector('form');
 const card = document.querySelector('.card');
 const info = document.querySelector('.info');
-const time = document.querySelector('.details img');
+const time = document.querySelector('.time');
 const icon = document.querySelector('.icon img');
 
 
+inputCity.addEventListener('submit', e => {
+    e.preventDefault();
 
-const updatedInfo = (data) => {
-    console.log(data);
-    const cityInfo = data.cityInfo;
-    const weather = data.weather;
+    const city = inputCity.city.value.trim();
+    inputCity.reset();
+
+    newCity(city)
+        .then(data => updateUI(data))
+        .catch(err => updateUI(err))
+});
+
+const newCity = async (city) => {
+    
+    const cityInfo = await cityCode (city);
+    const weather = await weatherInfo (cityInfo.Key);
+
+    return {cityInfo , weather};
+};
+
+const updateUI = (data) => {    
+    //Destrucure properties
+    const {cityInfo, weather} = data;
 
     info.innerHTML = `
     <h4 class="text-uppercase text-center">${cityInfo.EnglishName}</h4>
@@ -25,33 +42,9 @@ if (card.classList.contains('d-none')){
 const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
 icon.setAttribute('src',iconSrc);
 
-let timeSrc = null;
-if(weather.IsDayTime){
-    timeSrc = 'img/day.svg';
-} else {
-    timeSrc = 'img/night.svg';
-}
+let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
 time.setAttribute('src',timeSrc);
 
 };
 
-const newCity = async (city) => {
-    
-    const cityInfo = await cityCode (city);
-    const weather = await weatherInfo (cityInfo.Key);
-
-    return {cityInfo , weather};
-};
-
-inputCity.addEventListener('submit', e => {
-
-    e.preventDefault();
-
-    const city = inputCity.city.value.trim();
-    inputCity.reset();
-
-    newCity(city)
-        .then(data => updatedInfo(data))
-        .catch(err => updatedInfo(err))
-});
 
